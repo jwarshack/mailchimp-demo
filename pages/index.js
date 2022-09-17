e357d857ccf442957d1cf2d3de8720127fa59ad9
Orlundo Hubbard <orlundo@wursta.com>
2022-09-17 11:58:28 -0500

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -46,14 +46,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <form onSubmit={subscribe}>
